7b36c2fd2d33cc609b230ff0e391d7b97595308d
wkemrm <wkemrm12@naver.com>
2024-09-20 16:23:11 +0900

    [추가] 유저 create 기능 추가

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.RequestUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+    private final Environment env;
+    private final UserService userService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
+    }
+
+    @GetMapping("/users")
+    public String createUser(@RequestBody RequestUser requestUser) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(requestUser, UserDto.class);
+
+        userService.createUser(userDto);
+        return "Create user method is called";
+    }
+}
